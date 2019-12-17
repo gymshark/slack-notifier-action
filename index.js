1054eb5092755fae8472d04ebb3f143da1a602cb
Dan Lake <d.lake@gymshark.com>
2019-12-17 17:21:35 +0000

    Add exiting notifier code from IRIS repo

---
+++ index.js
@@ -0,0 +1,54 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const slack = require('slack-notify')(core.getInput('webhook_url'));
+const channel = core.getInput('channel');
+const job = JSON.parse(core.getInput('job'));
+try {
+    // Get the JSON webhook payload for the event that triggered the workflow
+
+    //console.log(`The event payload: ${job}`);
+
+    let payload;
+
+    if (job.status === "Failure"){
+        payload = {
+            pretext: "Deployment failure",
+            color: "#C0392A",
+            icon: ":red_circle:",
+        }
+    } else if (job.status === "Success"){
+        payload = {
+            pretext: "Deployment Success",
+            color: "#27ae60",
+            icon: ":green_circle:",
+        }
+    } else if (job.status === "Cancelled"){
+        payload = {
+            pretext: "Deployment Canceled",
+            color: "#FEEFAB",
+            icon: ":yellow_circle:",
+        }
+    }
+
+    slack.send({
+        icon_emoji: payload.icon,
+        username: 'Github Actions',
+        attachments: [
+            {
+                fallback: 'Required Fallback String',
+                pretext: payload.pretext,
+                author_name: github.context.payload.repository.full_name,
+                author_link: `https://github.com/${github.context.payload.repository.full_name}/`,
+                title: github.context.payload.ref,
+                title_link: `https://github.com/${github.context.payload.repository.full_name}/actions`,
+                text: github.context.payload.repository.description,
+                color: payload.color,
+                fields: [
+                    { title: 'Job Status', value: job.status, short: false },
+                ]
+            }
+        ]
+    });
+} catch (error) {
+    core.setFailed(error.message);
+}
